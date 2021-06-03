8db192e826456266ec0c7917c29dc789b4ad7002
dshwang1 <yoonseok115@gmail.com>
2021-06-02 23:11:28 -0400

    Update LibraryServlet.java

--- src/main/java/com/cognixia/jump/LibraryProject/web/LibraryServlet.java
+++ src/main/java/com/cognixia/jump/LibraryProject/web/LibraryServlet.java
@@ -1,30 +1,49 @@
 package com.cognixia.jump.LibraryProject.web;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cognixia.jump.LibraryProject.connection.ConnectionManager;
+import com.cognixia.jump.LibraryProject.dao.BookDAO;
+import com.cognixia.jump.LibraryProject.dao.LibrarianDAO;
+import com.cognixia.jump.LibraryProject.dao.PatronDAO;
+
 /**
  * Servlet implementation class LibraryServlet
  */
 public class LibraryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private BookDAO bookDao;
+	private PatronDAO patronDao;
+	private LibrarianDAO librarianDao;
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		bookDao = new BookDAO();
+		patronDao = new PatronDAO();
+		librarianDao = new LibrarianDAO();
 	}
 
 	/**
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		try {
+			ConnectionManager.getConnection().close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
